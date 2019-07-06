56d546336a5978f3c7ebff494e3734ea6d6655b2
herinjaka <herinjaka.rakotoarivony@gmail.com>
2019-07-06 16:16:06 +0200

    Add confirm delete

--- application/views/home/vw_home.php
+++ application/views/home/vw_home.php
@@ -31,11 +31,35 @@
                 <td scope="row"><?=$oSong->song_link_mp3?></td>
                 <td scope="row"><?=$oSong->song_description?></td>
                 <td scope="row" style="text-align: center">
-                    <a href="<?=base_url('home/update-song-page/').$oSong->song_id?>" class="btn btn-sm btn-primary"><i class="fa fa-pencil"></i></a>
-                    <a href="<?=base_url('home/delete-song/').$oSong->song_id?>" class="btn btn-sm  btn-danger"><i class="fa fa-trash-o"></i></a>
+                    <a href="<?=base_url('home/update-song-page/').$oSong->song_id?>" class="btn btn-sm btn-primary">
+                        <i class="fa fa-pencil"></i>
+                    </a>
+                    <a data-href="<?=base_url('home/delete-song/').$oSong->song_id?>"
+                       data-toggle="modal" data-target="#confirm-delete"
+                       class="btn btn-sm btn-danger">
+                        <i class="fa fa-trash-o"></i>
+                    </a>
                 </td>
             </tr>
         <?php endforeach;?>
         </tbody>
     </table>
+</div>
+
+<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Suppression</h4>
+            </div>
+            <div class="modal-body">
+                Voulez-vous vraiment supprimer cet élément ?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Annuler</button>
+                <a class="btn btn-danger btn-ok  btn-sm">Valider</a>
+            </div>
+        </div>
+    </div>
 </div>
--- assets/my_js/home.js
+++ assets/my_js/home.js
@@ -1,3 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#dataTableHome').DataTable();
-} );
+
+    $('#confirm-delete').on('show.bs.modal', function(e) {
+        $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+    });
+});
